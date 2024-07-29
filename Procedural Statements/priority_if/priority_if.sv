e4084726d3203e9f7c682152fbec5c541f892b44
MAGNUS ANTONY <magnusantony09@gmail.com>
2024-07-30 00:17:50 +0530

    Create priority_if.sv

---
+++ Procedural Statements/priority_if/priority_if.sv
@@ -0,0 +1,43 @@
+module priority_if;// Gives Priority to First True Expression
+  bit[3:0]a,b;
+  initial begin
+    a=10;
+    b=15;
+    priority if(a<b)begin
+      $display("a is less than b");
+    end
+    else if(a==10)begin
+      $display("a is equal to 10");
+    end
+    else begin
+      $display("a is equal to b");
+    end
+  end
+endmodule
+
+
+OUTPUT:
+
+Top Level Modules:
+       priority_if
+TimeScale is 1 ns / 1 ns
+Starting vcs inline pass...
+
+1 module and 0 UDP read.
+recompiling module priority_if
+rm -f _cuarc*.so _csrc*.so pre_vcsobj_*.so share_vcsobj_*.so
+if [ -x ../simv ]; then chmod a-x ../simv; fi
+g++  -o ../simv      -rdynamic  -Wl,-rpath='$ORIGIN'/simv.daidir -Wl,-rpath=./simv.daidir -Wl,-rpath=/apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib -L/apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib  -Wl,-rpath-link=./   objs/amcQw_d.o   _332_archive_1.so   SIM_l.o       rmapats_mop.o rmapats.o rmar.o rmar_nd.o  rmar_llvm_0_1.o rmar_llvm_0_0.o            -lvirsim -lerrorinf -lsnpsmalloc -lvfs    -lvcsnew -lsimprofile -luclinative /apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib/vcs_tls.o   -Wl,-whole-archive  -lvcsucli    -Wl,-no-whole-archive          /apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib/vcs_save_restore_new.o -ldl  -lc -lm -lpthread -ldl 
+../simv up to date
+CPU time: .431 seconds to compile + .422 seconds to elab + .375 seconds to link
+Chronologic VCS simulator copyright 1991-2023
+Contains Synopsys proprietary information.
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Jul 29 14:46 2024
+  
+a is less than b
+
+  
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 0 ns
+CPU Time:      0.440 seconds;       Data structure size:   0.0Mb
+Mon Jul 29 14:46:11 2024
