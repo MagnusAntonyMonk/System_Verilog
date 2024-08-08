3077e6c50514db8f32654c63059b2e4f27c9d25e
MAGNUS ANTONY <magnusantony09@gmail.com>
2024-08-08 11:28:38 +0530

    Delete Classes and OOPS/D FLIPFLOP directory

--- Classes and OOPS/D FLIPFLOP/interface.sv
+++
@@ -1,6 +0,0 @@
-interface create;
-  logic clk;
-  logic rst_n;
-  logic d;
-  bit q; 
-endinterface
--- Classes and OOPS/D FLIPFLOP/transaction.sv
+++
@@ -1,6 +0,0 @@
-class transaction;
-  rand logic d;
-  rand bit clk;
-  rand bit rst_n;
-  bit q;
-endclass
