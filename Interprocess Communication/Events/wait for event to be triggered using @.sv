4d3c6bf612a84005373ab6ddb2bb7cdd2f6f8646
MAGNUS ANTONY <magnusantony09@gmail.com>
2024-09-12 08:07:41 +0200

    Create wait for event to be triggered using @.sv

---
+++ Interprocess Communication/Events/wait for event to be triggered using @.sv
@@ -0,0 +1,38 @@
+module event_example;
+  event a1;
+  task a();
+    #10;
+    $display("before triggering an event");
+    ->a1;
+    $display("after triggering an event");
+  endtask
+   task b();
+    $display("waiting for triggering an event");
+    @a1;
+     $display("triggering an event is done");
+  endtask
+  initial begin
+    fork
+      a();
+      b();
+    join
+  end
+endmodule
+
+
+OUTPUT:
+
+
+Top level design units:
+		event_example
+Loading snapshot worklib.event_example:sv .................... Done
+xcelium> source /xcelium23.09/tools/xcelium/files/xmsimrc
+xcelium> run
+
+waiting for triggering an event
+before triggering an event
+after triggering an event
+triggering an event is done
+  
+xmsim: *W,RNQUIE: Simulation is complete.
+xcelium> exit
