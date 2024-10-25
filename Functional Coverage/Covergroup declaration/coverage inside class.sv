cefcb3d143b474ada267ac912feff98bf7f9fed9
MAGNUS ANTONY <magnusantony09@gmail.com>
2024-10-25 12:00:21 +0530

    Create coverage inside class.sv

---
+++ Functional Coverage/Covergroup declaration/coverage inside class.sv
@@ -0,0 +1,31 @@
+class pack;
+rand bit [1:0]a;
+rand bit [2:0]b;
+endclass
+pack p =new;
+
+module cvgrp_inside_mod;
+covergroup cgrp ;
+  c1: coverpoint p.a; 
+  c2: coverpoint p.b ;
+endgroup 
+cgrp c;
+initial begin
+c=new;
+repeat (5) begin
+  void'(p.randomize);
+  c.sample();
+$display ("a=%d ; b=%d ; coverage %%=%.2f%%",p.a,p.b,c.get_inst_coverage());
+end
+$finish;
+end
+endmodule
+
+OUTPUT:
+
+# run -all
+# a=3 ; b=2 ; coverage %=18.75%
+# a=0 ; b=1 ; coverage %=37.50%
+# a=2 ; b=4 ; coverage %=56.25%
+# a=1 ; b=2 ; coverage %=68.75%
+# a=0 ; b=0 ; coverage %=75.00%
